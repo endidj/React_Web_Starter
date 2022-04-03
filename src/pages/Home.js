fcc278f4950c5049d0a1eef3de988bdc2c39eaa4
jksim <immadtoo99@gmail.com>
2022-04-03 14:35:17 +0900

    초기엔 js파일만 존재 (tsx -> js)

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const Home = () => {
+    return (
+        <>
+            <div>홈화면</div>
+        </>
+    )
+}
--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Home= () => {
-    return (
-        <div>홈화면</div>
-    )
-}
